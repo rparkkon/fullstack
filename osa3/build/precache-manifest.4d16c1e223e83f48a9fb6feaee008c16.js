c1bb80e1145bbbbd8f51e1e2de0e7917f3b91422
Risto Parkkonen <risto.parkkonen@wmail.fi>
2018-11-21 12:04:50 +0200

    Osa 3.12-18 f

---
+++ osa3/build/precache-manifest.4d16c1e223e83f48a9fb6feaee008c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f0d97f0a11bea098a5a",
+    "url": "/static/js/main.1f0d97f0.chunk.js"
+  },
+  {
+    "revision": "3b1846dbd9e8365c15a3",
+    "url": "/static/js/1.3b1846db.chunk.js"
+  },
+  {
+    "revision": "1f0d97f0a11bea098a5a",
+    "url": "/static/css/main.f44d89e2.chunk.css"
+  },
+  {
+    "revision": "836f1f14c3251da42488c7842795c3bd",
+    "url": "/index.html"
+  }
+];
